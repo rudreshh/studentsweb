23ee202faf055e164e33ebcd9eabd764fbdcaf6e
Kuldeep Jain <kuldipjain2002@gmail.com>
2021-10-15 03:40:46 +0530

    added final feedback form

--- feedback form final-2/script.js
+++ feedback form final-2/script.js
@@ -17,12 +17,22 @@ var contact = document.getElementById('ContactNo');
 // document.querySelector("#send").addEventListener("click", validation())
 
 function validation(){
-  var x = document.getElementById("star-5").required;
-  var name = document.getElementById('name');
-  var email = document.getElementById('email');
-  var contact = document.getElementById('ContactNo');
 
-if(x==false || name=="" || email=="" || contact==""){
-    alert("Please fill all the required fields");
+  var s1= document.getElementById("star-1").checked;
+  var s2= document.getElementById("star-2").checked;
+  var s3= document.getElementById("star-3").checked;
+  var s4= document.getElementById("star-4").checked;
+  var s5= document.getElementById("star-5").checked;
+  if(s1||s2||s3||s4||s5){
+    var x =true;
   }
+  else{
+    var x = false;
+  }
+  if(x==false){
+
+
+    alert("Please rate us");
+    }
+
 }
